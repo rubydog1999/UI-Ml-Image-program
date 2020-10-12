74c460637317cdadacf2d1dd7d81cdfa33692ec6
rubydog1999 <trungdzung9900@gmail.com>
2020-10-12 11:29:32 +0700

    reformat code

--- App.py
+++ App.py
@@ -14,12 +14,11 @@ import cv2
 import numpy as np
 from keras.models import load_model
 
-
-
 root = Tk()
 root.title("Machine Learning App")
 root.iconbitmap('d:/Pycharm/pycharmproject/Clustering/Icon.ico')
 root['background'] = "#856ff8"
+
 
 class FullScreenApp(object):
     def __init__(self, master, **kwargs):
@@ -40,6 +39,8 @@ class FullScreenApp(object):
 app = FullScreenApp(root)
 root.bind('<Escape>', lambda event: exitApp())
 root.bind('<F11>', lambda event: root.state('zoomed'))
+
+
 def doNothing():
     myLabel = Label(root, text="button click")
     myLabel.place(re)
@@ -52,16 +53,17 @@ def doDescription(value):
                                               " We will choose any image and enter the number k (number of clusters) we want\n"
                                               " to process. The program calculates and labels the pixels according to \n "
                                               "the k-mean formula and produces a table of results\n (Cluster, Pixel RGB, Pixel XY and Distance from the point to center mass)",
-                      bg="black",fg = "white")
+                      bg="black", fg="white")
         myDes.place(relwidth=1, relheight=1)
     elif value == "Image segmentation":
-        myDes = Label(description_layer, text="Image Segmentation involves converting an image into a collection of regions \n of pixels"
-                                              " that are represented by a mask or a labeled image\n."
-                                              " By dividing an image into segments,\n"
-                                              " you can process only the important segments of the image\n"
-                                              " instead of processing the entire image. So here we get the k value and image\n"
-                                              "​then we use the k-mean clustering algorithm to perform image segmentation",
-                      bg = "black", fg = "white")
+        myDes = Label(description_layer,
+                      text="Image Segmentation involves converting an image into a collection of regions \n of pixels"
+                           " that are represented by a mask or a labeled image\n."
+                           " By dividing an image into segments,\n"
+                           " you can process only the important segments of the image\n"
+                           " instead of processing the entire image. So here we get the k value and image\n"
+                           "​then we use the k-mean clustering algorithm to perform image segmentation",
+                      bg="black", fg="white")
         myDes.place(relwidth=1, relheight=1)
     elif value == "Validation":
         myDes = Label(description_layer,
@@ -72,35 +74,45 @@ def doDescription(value):
                       bg="black", fg="white")
         myDes.place(relwidth=1, relheight=1)
 
+
 def exitApp():
     response = messagebox.askyesno("Machine Learning App", "Do you want exit")
     if response == 1:
         root.destroy()
 
+
 def openimg():
     global my_img
     global filename
     filename = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select A File", filetypes=(
-    ("jpg images", ".jpg"), ("png images", ".png"), ("all files", "*.*")))
+        ("jpg images", ".jpg"), ("png images", ".png"), ("all files", "*.*")))
     if not filename:
         return
     my_img = ImageTk.PhotoImage(Image.open(filename))
-    my_label_img = Label(imgae_layer, image=my_img, bg ="black")
+    my_label_img = Label(imgae_layer, image=my_img, bg="black")
     my_label_img.place(relwidth=1, relheight=1)
+
+
 root.protocol("WM_DELETE_WINDOW", exitApp)
+
+
 def check_table(table_cluster):
     if table_cluster is not None:
-            del table_cluster
+        del table_cluster
+
+
 def check_figure(figure):
     if figure is not None:
         del figure
+
+
 def run():
     global table_cluster
     table_cluster = 0
     global figure
     figure = 1
     values = clicked.get()
-    #run k-mean method
+    # run k-mean method
     if values == "K-mean":
         check_figure(figure)
         myRgraphic = Label(result_graphic, text="Result Graphic")
@@ -108,6 +120,7 @@ def run():
         k_val = int(e.get())
         image = cv2.imread(filename=filename)
         imgRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
         def distance(point1, point2):
             return sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2 + (point1[2] - point2[2]) ** 2)
 
@@ -179,8 +192,9 @@ def run():
                         if dist < min_distance:
                             min_distance = dist
                             best_cluster = center
-                    clusters[best_cluster].append((best_cluster,rgb1, [x, y], min_distance))
+                    clusters[best_cluster].append((best_cluster, rgb1, [x, y], min_distance))
             return clusters
+
         print()
         cl = k_means(img=imgRGB, k=k_val, max_iter=10)
         table_element_2 = []
@@ -190,10 +204,11 @@ def run():
         row = len(table_element_2)
         column_names = ["Cluster", "Pixel RGB", "pixel X Y", "Distance"]
         table_cluster = pandas.DataFrame(table_element_2, columns=column_names, index=range(row))
-        table_final = Table(result_text, dataframe=table_cluster,  showstatusbar=True)
+        table_final = Table(result_text, dataframe=table_cluster, showstatusbar=True)
         table_final.show()
 
-#run Image segmentation
+
+    # run Image segmentation
     elif values == "Image segmentation":
         check_table(table_cluster)
         myRtext = Label(result_text, text="ResultText")
@@ -218,6 +233,7 @@ def run():
         plt.title('Segmented Image K = %i' % K), plt.xticks([]), plt.yticks([])
         canvas = FigureCanvasTkAgg(figure, result_graphic)
         canvas.get_tk_widget().pack()
+
 
 
 
@@ -246,29 +262,29 @@ def run():
 
 
     else:
-        rep = messagebox.showwarning("Warning","Must choose method !")
-        Label(root,text = rep)
+        rep = messagebox.showwarning("Warning", "Must choose method !")
+        Label(root, text=rep)
+
 
 def exportCSV():
     values = clicked.get()
     if values == "K-mean":
-            if table_cluster is None:
-                rep = messagebox.showwarning("Warning", "No result to save")
-                Label(root, text=rep)
-            else:
-                export_file_path = filedialog.asksaveasfilename(defaultextension='.csv')
-                table_cluster.to_csv(export_file_path, index=True, header=True)
+        if table_cluster is None:
+            rep = messagebox.showwarning("Warning", "No result to save")
+            Label(root, text=rep)
+        else:
+            export_file_path = filedialog.asksaveasfilename(defaultextension='.csv')
+            table_cluster.to_csv(export_file_path, index=True, header=True)
     elif values == "Image segmentation":
-            if figure is None:
-                rep = messagebox.showwarning("Warning", "No result to save")
-                Label(root, text=rep)
-            else:
-                save_result_imgae = filedialog.asksaveasfilename(defaultextension='.png')
-                figure.savefig(save_result_imgae)
+        if figure is None:
+            rep = messagebox.showwarning("Warning", "No result to save")
+            Label(root, text=rep)
+        else:
+            save_result_imgae = filedialog.asksaveasfilename(defaultextension='.png')
+            figure.savefig(save_result_imgae)
     else:
         rep = messagebox.showwarning("Warning", "No result to save")
         Label(root, text=rep)
-
 
 
 # Main Menu
@@ -282,7 +298,6 @@ otherMenu = Menu(menu)
 menu.add_cascade(label="Other", menu=otherMenu)
 otherMenu.add_command(label="Exit", command=exitApp)
 
-
 # Toolbar
 toolbar = Frame(root, bg="#4267B2", padx=25, pady=20)
 inserBut = Button(toolbar, text="Open Image", command=openimg, padx=15, pady=15)
@@ -290,13 +305,13 @@ inserBut.pack(side=LEFT, padx=2, pady=2)
 runBut = Button(toolbar, text="Run", command=run, padx=15, pady=15)
 runBut.pack(side=LEFT, padx=30, pady=2)
 imp_kmean = Label(toolbar, text="Import number of K")
-imp_kmean.pack(side=LEFT,padx=5,pady=2,ipadx=20, ipady=5)
+imp_kmean.pack(side=LEFT, padx=5, pady=2, ipadx=20, ipady=5)
 e = Entry(toolbar, width=50)
-e.pack(side=LEFT, padx=2, pady=10,ipady =5,ipadx=20)
+e.pack(side=LEFT, padx=2, pady=10, ipady=5, ipadx=20)
 e.insert(0, "")
 clicked = StringVar()
 clicked.set("Method")
-options = ["K-mean", "Image segmentation","Validation"]
+options = ["K-mean", "Image segmentation", "Validation"]
 drop = OptionMenu(toolbar, clicked, *options, command=doDescription)
 drop.config(width=30, height=2)
 drop.pack(side=RIGHT, padx=2, pady=2)
